7d6d57aa2f38c7836e908ceafb756618ed781688
bavecs <balazsvecsei9@gmail.com>
2023-05-11 10:04:15 +0200

    init

---
+++ tictactoe/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
+  )
+}
